e72677fb1e95d5c1c6e993436bbe1e37cbe6505b
vgiuliani11 <vinicius.giuliani@gmail.com>
2024-04-10 23:17:16 -0300

    Anonimização do endpoint das contribuições para o mapa da função social

--- wp-content/themes/mapacolaborativo-1.0/inc/persistence/funcao_social/ColabImovelDAO.php
+++ wp-content/themes/mapacolaborativo-1.0/inc/persistence/funcao_social/ColabImovelDAO.php
@@ -42,32 +42,12 @@ class ColabImovelDAO implements IColabImovelDAO{
             , MAX(IF(field_name='plataformaApoioNumeroApoios', field_value, NULL )) AS 'plataformaApoioNumeroApoios'
             , MAX(IF(field_name='tempo_sem_utilizacao', field_value, NULL )) AS 'tempoInutilizado'
             FROM wp_cf7dbplugin_submits WHERE form_name = '" . $this->formColab . "' GROUP BY submit_time) 
-            AS A WHERE A.plataformaStatus = 'S') AS B JOIN (SELECT
-            DATE_FORMAT(FROM_UNIXTIME(submit_time), '%b %e, %Y  %l:%i %p') AS Submitted
-            , submit_time AS 'idUser'
-            , MAX(IF(field_name='nome', field_value, NULL )) AS 'usuarioNome'
-            , MAX(IF(field_name='emailCadastro', field_value, NULL )) AS 'usuarioEmail'
-            , MAX(IF(field_name='instituicao', field_value, NULL )) AS 'usuarioInstituicao'
-            , MAX(IF(field_name='endereco', field_value, NULL )) AS 'usuarioEndereco'
-            , MAX(IF(field_name='datanasc', field_value, NULL )) AS 'datanasc'
-            , MAX(IF(field_name='CEP', field_value, NULL )) AS 'usuarioCEP'
-            FROM wp_cf7dbplugin_submits WHERE form_name = '" . $this->formUser . "' GROUP BY submit_time)"
-            . " AS C ON B.author_mail = C.usuarioEmail";
+            AS A WHERE A.plataformaStatus = 'S') AS B;";
         global $wpdb;
         $results = $wpdb->get_results($query);
         foreach ($results as $row){
             $colaboracao = new ColabImovel();
-            $autor = new Usuario();
-            $autor->id = $row->idUser;
-            $autor->nome = $row->usuarioNome;
-            $autor->email = $row->usuarioEmail;
-            $autor->instituicao = $row->usuarioInstituicao;
-            $autor->endereco = $row->usuarioEndereco;
-            $autor->cep = $row->usuarioCEP;
-            $autor->datanasc = $row->datanasc;
             $colaboracao->id = $row->submit_time;
-            $colaboracao->autor = $autor;
-            $colaboracao->author_email = $row->author_mail;
             $colaboracao->latitude = $row->latitude;
             $colaboracao->longitude = $row->longitude;
             $colaboracao->logradouro = $row->logradouro;
